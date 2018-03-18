64c3df1a5063164fa43d4d48e97211202818c156
Nicolas A Perez <anicolaspp@gmail.com>
2018-03-18 18:02:02 -0400

    Simplifying

--- scala/sieve/src/main/scala/Sieve.scala
+++ scala/sieve/src/main/scala/Sieve.scala
@@ -1,26 +1,18 @@
 object Sieve {
+  
+  def primes(n: Int) = from(2, n, (2 to n).toList)
 
-
-  def primes(n: Int) = loop(n)
-
-
-  def loop(n: Int) = {
-
-    def from(k: Int, to: Int, result: List[Int]): List[Int] = {
-
-      if (k > n) {
-        result
-      } else {
-        result.filterNot(v => v > k && v % k == 0) match {
-          case Nil    =>  Nil
-          case primes =>  primes.find(_ > k) match {
-            case Some(value) => from(value, to, primes)
-            case None        => primes
-          }
+  def from(k: Int, to: Int, result: List[Int]): List[Int] =
+    if (k > to) {
+      result
+    } else {
+      result.filterNot(v => v > k && v % k == 0) match {
+        case Nil    =>  Nil
+        case primes =>  primes.find(_ > k) match {
+          case Some(value)  =>  from(value, to, primes)
+          case None         =>  primes
         }
       }
     }
 
-    from(2, n, (2 to n).toList)
-  }
 }
